d8a36e915700dee4c3bc6a12d2dfd3360c3ca7a7
Dan Otto <theDanOtto@gmail.com>
2016-01-26 19:56:42 -0700

    evaluate hands

--- lib/hand.rb
+++ lib/hand.rb
@@ -5,8 +5,16 @@ class Hand
     @cards = cards
   end
 
-  def valid_hand?
-    self.cards.length == 5
+  def card_values
+    values = []
+    self.cards.each do |card|
+      values << card[0]
+    end
+    return values
+  end
+
+  def hand_type
+    
   end
 
   def suits
@@ -16,12 +24,108 @@ class Hand
     end
     return suits
   end
+  
+  def valid_hand?
+    self.cards.length == 5
+  end
 
-  def card_values
-    values = []
-    self.cards.each do |card|
-      values << card[0]
+  def straight_flush?
+    self.flush? && self.straight?
+  end 
+
+  def four_of_a_kind?
+    number_of_pairs == 1 && occurances_in_pair == 4
+  end 
+
+  def full_house?
+    number_of_pairs == 2 && occurances_in_pair == 3
+  end 
+
+  def flush?
+    self.suits.uniq.one?
+  end 
+
+  def straight?
+    aces_low_straight? || aces_high_straight?
+  end 
+  
+  def three_of_a_kind?
+    number_of_pairs == 1 && occurances_in_pair == 3
+  end 
+
+  def two_pair?
+    number_of_pairs == 2 && occurances_in_pair == 2
+  end 
+
+  def pair?
+    number_of_pairs == 1 && occurances_in_pair == 2
+  end 
+
+  def high_card?
+    true 
+  end 
+
+  def number_of_pairs
+    card_occurences = self.card_values.each_with_object(Hash.new(0)) { |card, counts| counts[card] += 1 }
+    instances_of_pairs = card_occurences.to_a.select do |arr|
+      arr[1] > 1
     end
-    return values
+    instances_of_pairs.count
+  end
+
+  def occurances_in_pair
+    instances = []
+    card_occurances = self.card_values.each_with_object(Hash.new(0)) { |card, counts| counts[card] += 1 }
+    card_occurances.each do |arr|
+      instances << arr[1]
+    end
+    instances.max
+  end
+
+  def aces_low_straight?
+    your_card_rankings = self.card_rankings_sorted(ace_value: 1)
+    self.straight_generator(your_card_rankings[0]) == your_card_rankings
+  end
+
+  def aces_high_straight?
+    your_card_rankings = self.card_rankings_sorted
+    self.straight_generator(your_card_rankings[0]) == your_card_rankings
+  end
+
+  def straight_generator(lowest_card_value)
+    straight_tester = [lowest_card_value]
+    4.times do |num|
+      straight_tester << lowest_card_value + num + 1
+    end
+    straight_tester
+  end
+
+  def card_values_ranked(ace_value: 14)
+    card_rankings = {
+      "A" =>  ace_value,
+      "K" => 13,
+      "Q" =>  12,
+      "J" =>  11,
+      "T" =>  10,
+      "9" =>  9,
+      "8" => 8,
+      "7" => 7,
+      "6" => 6,
+      "5" => 5,
+      "4" => 4,
+      "3" => 3,
+      "2" => 2
+    }
+    @card_values ||= self.card_values
+    card_ranks = []
+    @card_values.each do |card|
+      card_ranks << card_rankings[card]
+    end
+    card_ranks
+  end
+
+  def card_rankings_sorted(ace_value: 14)
+    your_card_rankings = self.card_values_ranked(ace_value: ace_value)
+    your_card_rankings.sort
   end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -2,8 +2,175 @@ require "spec_helper"
 require "hand"
 
 describe Hand do
+  def cards(hand_type)
+    available_hands = {
+      straight_flush: %w(9S TS 8S 7S JS),
+      four_of_a_kind: %w(TS TC TH 5D TD),
+      full_house: %w(TS TC TH 5D 5C),
+      flush: %w(AS TS 9S 3S 2S),
+      ace_high_straight: %w(AS TD JC QH KC),
+      straight: %w(9S TS 8D 7S JS),
+      ace_low_straight: %w(AS 2H 3D 5C 4S),
+      three_of_a_kind: %w(AC AS AD 3H 2D),
+      two_pair: %w(AC AS 3H 3D 2D),
+      pair: %w(KC KD 3H JD TS),
+      high_card: %w(KC QD TC 4D 5S)
+    }
+    return available_hands[hand_type]
+  end
   # all the tests for the hand
-  describe "#is_a_hand?" do
+
+  describe "#straight_flush?" do
+    it "recognizes Straight Flushes!" do
+      hand = Hand.new(cards(:straight_flush))
+
+      expect(hand.straight_flush?).to eq(true)
+    end
+  end
+
+  describe "#four_of_a_kind?" do
+    it "returns true for a hand with a 4 of a kind" do
+      hand = Hand.new(cards(:four_of_a_kind))
+
+      expect(hand.four_of_a_kind?).to eq(true)
+    end
+
+    it "returns false for 3 of a kind" do
+      hand = Hand.new(cards(:three_of_a_kind))
+
+      expect(hand.four_of_a_kind?).to eq(false)
+    end
+  end
+
+  describe "#full_house?" do
+    it "returns true when a full house exists" do
+      hand = Hand.new(cards(:full_house))
+
+      expect(hand.full_house?).to eq(true)
+    end
+
+    it "returns false when two pair" do
+      hand = Hand.new(cards(:two_pair))
+
+      expect(hand.full_house?).to eq(false)
+    end
+  end
+
+  describe "#flush?" do
+    it "returns true when hand is flush" do
+      hand = Hand.new(cards(:flush))
+      
+      expect(hand.flush?).to eq(true)
+    end
+    it "returns false when it is not a flush" do
+      hand = Hand.new(cards(:straight))
+      
+      expect(hand.flush?).to eq(false)
+    end
+  end
+
+  describe "#straight?" do
+    it "returns false on non straights" do
+      hand = Hand.new(cards(:flush))
+      
+      expect(hand.straight?).to eq(false)
+    end
+
+    it "recognizes normal straights to be true" do
+      hand = Hand.new(cards(:straight))
+
+      expect(hand.straight?).to eq(true)
+    end
+
+    it "recognizes low straights starting with an Ace" do
+      hand = Hand.new(cards(:ace_low_straight))
+
+      expect(hand.straight?).to eq(true)
+    end
+
+    it "recognizes high straights ending with Ace" do
+      hand = Hand.new(cards(:ace_high_straight))
+
+      expect(hand.straight?).to eq(true)
+    end
+  end
+
+  describe "#three_of_a_kind?" do
+    it "returns true for a hand with a 3 of a kind" do
+      hand = Hand.new(cards(:three_of_a_kind))
+
+      expect(hand.three_of_a_kind?).to eq(true)
+    end
+
+    it "returns false for 4 of a kind" do
+      hand = Hand.new(cards(:four_of_a_kind))
+
+      expect(hand.three_of_a_kind?).to eq(false)
+    end
+  end
+
+  describe "#two_pair?" do
+    it "returns false for full house" do
+      hand = Hand.new(cards(:full_house))
+
+      expect(hand.two_pair?).to eq(false)
+    end
+
+    it "returns true for two pair" do
+      hand = Hand.new(cards(:two_pair))
+
+      expect(hand.two_pair?).to eq(true)
+    end
+
+    it "returns false for 4 of a kind" do
+      hand = Hand.new(cards(:four_of_a_kind))
+
+      expect(hand.two_pair?).to eq(false)
+    end
+  end
+
+  describe "#number_of_pairs" do
+    it "returns the 1 for a four of a kind" do
+      hand = Hand.new(cards(:four_of_a_kind))
+      
+      expect(hand.number_of_pairs).to eq(1)
+    end
+
+    it "returns 2 for full house" do
+      hand = Hand.new(cards(:full_house))
+
+      expect(hand.number_of_pairs).to eq(2)
+    end
+
+    it "returns 2 for two pair" do
+      hand = Hand.new(cards(:two_pair))
+
+      expect(hand.number_of_pairs).to eq(2)
+    end
+  end
+
+  describe "#occurances_in_pair" do
+    it "returns 4 for four of a kind" do
+      hand = Hand.new(cards(:four_of_a_kind))
+
+      expect(hand.occurances_in_pair).to eq(4)
+    end
+
+    it "returns 3 for full house" do
+      hand = Hand.new(cards(:full_house))
+
+      expect(hand.occurances_in_pair).to eq(3)
+    end
+
+    it "returns 2 for two pair" do
+      hand = Hand.new(cards(:two_pair))
+
+      expect(hand.occurances_in_pair).to eq(2)
+    end
+  end
+
+
+  describe "#valid_hand?" do
     it "a collection of 5 cards should be a hand" do
       hand = Hand.new(["AS", "KD", "QH", "JC", "TD"])
     
@@ -30,6 +197,38 @@ describe Hand do
       hand = Hand.new(["AS", "TD", "QH"])
 
       expect(hand.card_values).to eq(["A", "T", "Q"])
+    end
+  end
+
+  describe "#card_values_ranked(ace_value: 14)" do
+    it "sorts the card_values and ranks them from 1-14" do
+      # I wrote in a hand, rather than grabbing a method from above because I thought it was important to see the values all on one screen
+      hand = Hand.new(["AS", "TD", "QH", "KH", "9H"])
+
+      expect(hand.card_values_ranked).to eq([14, 10, 12, 13, 9])
+    end
+  end
+
+  describe "#card_rankings_sorted(ace_value: 14)" do
+    it "returns a sorted array of card rankings" do
+      hand = Hand.new(["AS", "TD", "QH", "KH", "9H"])
+      
+      expect(hand.card_rankings_sorted).to eq([9, 10, 12, 13, 14])
+    end
+
+    it "returns a value of 1 for for aces, when specified" do
+      hand = Hand.new(["AS", "TD", "QH", "KH", "9H"])
+
+      expect(hand.card_rankings_sorted(ace_value: 1)).to eq([1, 9, 10, 12, 13])
+    end
+  end
+
+
+  describe "#straight_generator" do
+    it "should create a 5 card array in order" do
+      initial_card_rank = 4
+      
+      expect(Hand.new("").straight_generator(initial_card_rank)).to eq([4, 5, 6, 7, 8])
     end
   end
 end
